325bcf14b2fdc192f97f795fe5e9f62b3ebacf9d
aflor018 <31411030+aflor018@users.noreply.github.com>
2021-08-05 08:37:10 -0700

    Add files via upload

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -38,18 +38,6 @@ catch {
 }
 #endregion
 
-#region Sysprep Fix
-# Fix for first login delays due to Windows Module Installer
-try {
-    ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
-    write-log "Sysprep Mode:VM fix applied"
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error updating script: $ErrorMessage"
-}
-#endregion
-
 #region Time Zone Redirection
 $Name = "fEnableTimeZoneRedirection"
 $value = "1"
